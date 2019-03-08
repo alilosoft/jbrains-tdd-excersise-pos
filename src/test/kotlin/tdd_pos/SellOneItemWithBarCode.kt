63fea49cbfb6c52bf37ac00cb7e8bd933d92e3c9
@lilo$oft <fellahi.ali@gmail.com>
2019-03-08 18:47:10 +0100

    adds feature: sell each item with correct price

--- src/test/kotlin/tdd_pos/SellOneItemWithBarCode.kt
+++ src/test/kotlin/tdd_pos/SellOneItemWithBarCode.kt
@@ -6,24 +6,33 @@ import org.junit.jupiter.api.Test
 class SellOneItemWithBarCode{
     @Test
     fun `when barcode exist display the price`() {
-        val pos = PointOfSale()
-        val barCode = "123456"
-        pos.onBarCode(barCode)
-
         val display = Display()
-        display.lastPrice() shouldBe "$10.99"
+        val pos = PointOfSale(display)
+
+        pos.onBarCode("123456")
+        display.lastText shouldBe "$10.99"
+
+        pos.onBarCode("123444")
+        display.lastText shouldBe "$5.99"
+
+        pos.onBarCode("123455")
+        display.lastText shouldBe "$11.99"
     }
 }
 
 class Display {
-    fun lastPrice(): String {
-        return "$10.99"
-    }
-
+    var lastText = "Hello"
 }
 
-class PointOfSale {
-    fun onBarCode(barCode: String) {
+class PointOfSale(private val display: Display) {
 
+    private val items = mapOf(
+        "123456" to "$10.99",
+        "123444" to "$5.99",
+        "123455" to "$11.99"
+    )
+
+    fun onBarCode(barCode: String) {
+        display.lastText = items[barCode] ?: "Unknown"
     }
 }
