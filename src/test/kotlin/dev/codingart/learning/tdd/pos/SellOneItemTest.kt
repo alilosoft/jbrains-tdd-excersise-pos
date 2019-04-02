97e45fc75ab3b13923fa7c3fc633a88c3e86424c
@lilo$oft <fellahi.ali@gmail.com>
2019-04-03 00:04:37 +0100

    Moves test fixtures to class level

--- src/test/kotlin/dev/codingart/learning/tdd/pos/SellOneItemTest.kt
+++ src/test/kotlin/dev/codingart/learning/tdd/pos/SellOneItemTest.kt
@@ -10,16 +10,19 @@ class SellOneItemTest {
 
     private fun emptyCatalogue() = Catalogue(mutableMapOf())
 
-    @ParameterizedTest(name = "barcode={0} -> price={1}")
-    @CsvSource("123456, $9.99", "123444, $4.99", "123455, $1.99")
-    fun `when barcode found then display the price`(barCode: String, price: String) {
-        val display = Display()
+    private val display = Display()
+    private val catalogue by lazy {
         val priceByBarcode = mutableMapOf(
             "123456" to "$9.99",
             "123444" to "$4.99",
             "123455" to "$1.99"
         )
-        val catalogue = Catalogue(priceByBarcode)
+        Catalogue(priceByBarcode)
+    }
+
+    @ParameterizedTest(name = "barcode={0} -> price={1}")
+    @CsvSource("123456, $9.99", "123444, $4.99", "123455, $1.99")
+    fun `when barcode found then display the price`(barCode: String, price: String) {
         val pos = SaleController(display, catalogue)
         pos.onBarCode(barCode)
         display.message shouldBe price
@@ -27,7 +30,6 @@ class SellOneItemTest {
 
     @Test
     fun `when barcode not found then display 'not found' message`() {
-        val display = Display()
         val pos = SaleController(display, emptyCatalogue())
         val barCode = "123456"
         pos.onBarCode(barCode)
@@ -35,11 +37,9 @@ class SellOneItemTest {
     }
 
 
-
     @ParameterizedTest
     @ValueSource(strings = [" ", "  ", " \n ", " \t "])
     fun `when barcode is blank then display error message`(barCode: String) {
-        val display = Display()
         val pos = SaleController(display, emptyCatalogue())
 
         pos.onBarCode(barCode)
