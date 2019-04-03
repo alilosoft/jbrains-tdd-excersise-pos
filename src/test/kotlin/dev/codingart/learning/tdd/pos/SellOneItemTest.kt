3b36066a9fb5c760c6ae813068f71b22b69994ba
@lilo$oft <fellahi.ali@gmail.com>
2019-04-03 12:49:00 +0100

    Reorganize test fixtures

--- src/test/kotlin/dev/codingart/learning/tdd/pos/SellOneItemTest.kt
+++ src/test/kotlin/dev/codingart/learning/tdd/pos/SellOneItemTest.kt
@@ -8,40 +8,38 @@ import org.junit.jupiter.params.provider.ValueSource
 
 class SellOneItemTest {
 
-    private fun emptyCatalogue() = Catalogue(mutableMapOf())
-
     private val display = Display()
+    private val emptyCatalogue by lazy { Catalogue(mutableMapOf()) }
     private val catalogue by lazy {
-        val priceByBarcode = mutableMapOf(
-            "123456" to "$9.99",
-            "123444" to "$4.99",
-            "123455" to "$1.99"
+        Catalogue(
+            mutableMapOf(
+                "123456" to "$9.99",
+                "123444" to "$4.99",
+                "123455" to "$1.99"
+            )
         )
-        Catalogue(priceByBarcode)
     }
+    private val saleController = SaleController(display, catalogue)
 
     @ParameterizedTest(name = "barcode={0} -> price={1}")
     @CsvSource("123456, $9.99", "123444, $4.99", "123455, $1.99")
     fun `when barcode found then display the price`(barCode: String, price: String) {
-        val pos = SaleController(display, catalogue)
-        pos.onBarCode(barCode)
+        saleController.onBarCode(barCode)
         display.message shouldBe price
     }
 
     @Test
     fun `when barcode not found then display 'not found' message`() {
-        val pos = SaleController(display, emptyCatalogue())
-        val barCode = "123456"
-        pos.onBarCode(barCode)
+        val barCode = "99999"
+        saleController.onBarCode(barCode)
         display.message shouldBe "Barcode $barCode not found"
     }
 
 
     @ParameterizedTest
-    @ValueSource(strings = [" ", "  ", " \n ", " \t "])
+    @ValueSource(strings = [" ", " \n ", " \t "])
     fun `when barcode is blank then display error message`(barCode: String) {
-        val pos = SaleController(display, emptyCatalogue())
-
+        val pos = SaleController(display, emptyCatalogue)
         pos.onBarCode(barCode)
         display.message shouldBe "Invalid Barcode"
     }
